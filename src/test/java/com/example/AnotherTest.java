c428b44cb70e2ab922674c8af198f2f89a802c9c
Simone Gianni <simone.gianni@aurea.com>
2017-01-31 19:13:18 +0100

    Added more tests and fork parameter to check if tests hang

---
+++ src/test/java/com/example/AnotherTest.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import org.junit.Test;
+
+/**
+ * Created by sym on 31/01/17.
+ */
+public class AnotherTest {
+    @Test
+    public void dummy1() throws Exception {
+        Thread.sleep(5000);
+        System.out.println("Dummy1 test executed");
+    }
+}
---
+++ src/test/java/com/example/YetAnotherTest.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import org.junit.Test;
+
+/**
+ * Created by sym on 31/01/17.
+ */
+public class YetAnotherTest {
+    @Test
+    public void dummy2() throws Exception {
+        Thread.sleep(5000);
+        System.out.println("Dummy2 test executed");
+    }
+}
